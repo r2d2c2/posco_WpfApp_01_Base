f1d21e89d3e913826fb3aa2e343ca7634d0c393e
r2d2c2 <uy6806@naver.com>
2025-05-20 18:28:36 +0900

    초기 커밋.

---
+++ WpfApp_01_Base/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_01_Base
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
