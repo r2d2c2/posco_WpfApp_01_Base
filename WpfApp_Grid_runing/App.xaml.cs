7cf8b6494d59527a2b7044a0356c81fee57d50f7
r2d2c2 <uy6806@naver.com>
2025-05-21 11:35:54 +0900

    Grid_runing add

---
+++ WpfApp_Grid_runing/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_Grid_runing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfApp_Grid_runing/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Grid_runing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IdInput.Text == "" || string.IsNullOrWhiteSpace(IdInput.Text)||string.IsNullOrWhiteSpace(PwInput.Text))
+            {
+                //MessageBox.Show("아이다,비밀번호를 입력해주세요.");
+                // 노랑 경고 창 생성
+                MessageBox.Show("아이디,비밀번호를 입력해주세요.", "경고", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show($"로그인 시도 {IdInput.Text}/{PwInput.Text}");
+            }
+
+        }
+    }
+}
